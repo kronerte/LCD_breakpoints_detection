e5c8d76e3d60175e11110514724e8b88e8da173f
kronerte <etienne.kronert@lilo.org>
2020-06-11 00:58:33 +0200

    test

--- docs/conf.py
+++ docs/conf.py
@@ -60,7 +60,7 @@ html_theme = 'alabaster'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
-
+# on précise la pprmière page de la doc
 master_doc = 'index'
-
+# theme
 html_theme = "sphinx_rtd_theme"
