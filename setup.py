b04547e05e5e536a3263851f70f6c15e2a1155b1
kronerte <etienne.kronert@lilo.org>
2020-06-11 10:16:00 +0200

    setup

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="LCD-breakpoints-detection", # Replace with your own username
+    version="0.1",
+    author="Etienne Kronert",
+    author_email="etienne.kronert@lilo.org",
+    description="Breakpoint Detection with LASSO and Coordinate Descent",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/kronerte/LCD_breakpoints_detection",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
